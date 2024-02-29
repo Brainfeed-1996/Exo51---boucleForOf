ab04c4f7eef4807078900bbdb2397a19d223a927
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-02-29 07:58:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// for of
+const tableau1 = ["a", "b", "c"];
+
+console.log(tableau1);
+
+for (const element of tableau1) {
+  console.log(element);
+}
+
+const tableau2 = [1, 2, 3];
+
+console.log(tableau2);
+
+for (const element of tableau2) {
+  console.log(element);
+}
+
+//Parcourir une chaîne de caractère
+
+let livre = "La Tour Sombre";
+
+for (const element of livre) {
+  console.log(element);
+}
+console.log(livre);
